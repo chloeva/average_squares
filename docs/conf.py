2e1402ffc00179da0597f563e93d64551be5d403
chloeva <chloe.allen.22@ucl.ac.uk>
2025-11-09 17:33:56 +0000

    Generating documentation with Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('week6/average_squares/docs'))
+
+project = 'exercise2'
+copyright = '2025, Chloeva'
+author = 'Chloeva'
+release = '2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. exercise2 documentation master file, created by
+   sphinx-quickstart on Sun Nov  9 16:28:04 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to exercise2 documentation!
+=================================================================================================
+average_squares is a Python project which calculates the average of squares of numbers in a list. 
+It is designed to be simple, fast, and easy to use
+for educational purposes or quick calculations.
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents
+   
+   content/average-squares-docs
